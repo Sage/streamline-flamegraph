effe8a3313cfd3e3d83dbc4d6b257ac0852dff94
Bruno Jouhier <bjouhier@gmail.com>
2014-09-15 16:37:00 +0200

    issue #6 - use long option names

--- bin/gen-graphs.sh
+++ bin/gen-graphs.sh
@@ -2,7 +2,7 @@
 
 BASE_PATH="$(dirname $0)/.."
 
-cat perf-recorded.gz | _node --cache "$BASE_PATH/lib/fold"  -o perf-folded.txt -n perf-attr.txt
+cat perf-recorded.gz | _node --cache "$BASE_PATH/lib/fold"  --output perf-folded.txt --nameattr perf-attr.txt
 [ -f palette.map ] || echo "io->rgb(0,255,255)" > palette.map
 cat perf-folded.txt | egrep -v -e '^node;pipe .*?;io \d+$' | "$BASE_PATH/deps/flamegraph.pl" --nameattr perf-attr.txt --cp $* > perf-full.svg
 cat perf-folded.txt | egrep -v -e ';io \d+$' | "$BASE_PATH/deps/flamegraph.pl" --nameattr perf-attr.txt --cp $* > perf-cpu.svg
